0f98856e73981a5062f8115b7349cadbe19866c7
Kelyn Crandall <kelyn@crandall.me>
2019-10-26 17:45:29 -0500

    Removed Django and migrated scraper code to flask

--- backend/restful/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Scraper
-
-
-class ScraperSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Scraper
-        fields = ('business_type', 'latitude', 'longitude', 'city', 'radius',)
